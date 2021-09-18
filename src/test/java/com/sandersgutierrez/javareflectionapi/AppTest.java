fc31a4a6c35413db343af94684006041dda252da
Sanders Gutiérrez <ing.sanders@gmail.com>
2021-09-18 11:50:25 -0300

    Fix imports at AppTest.
    Signed-off-by: Sanders Gutiérrez <ing.sanders@gmail.com>

--- src/test/java/com/sandersgutierrez/javareflectionapi/AppTest.java
+++ src/test/java/com/sandersgutierrez/javareflectionapi/AppTest.java
@@ -1,8 +1,8 @@
 package com.sandersgutierrez.javareflectionapi;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AppTest {
     @Test
